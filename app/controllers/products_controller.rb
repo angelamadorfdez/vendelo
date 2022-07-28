b1d8c3083000db2d1606e8fa7c3aa1d5ac091772
angelamadorfdez <angel@ingenio.xyz>
2022-07-28 17:16:01 +0200

    CRUD finished

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,14 +17,36 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params) # Le pasamos los parámetros que hemos indicado en el formulario.
 
     if @product.save
-      redirect_to products_path
+      redirect_to products_path, notice: 'Tu producto se ha creado correctamente.'
     else
       render :new, status: :unprocessable_entity # Devuelve código 422. Renderiza de nuevo el formulario. Necesario para devolver errores
     end
   end 
 
-  private
+  def edit
+    @product = Product.find(params[:id])
+  end
 
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to product_path(@product), notice: 'Tu producto se ha actualizado correctamente.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path, notice: 'Tu producto se ha eliminado correctamente.', status: :see_other # Status 303 (para que Turbo no se lie al cambiar el método PHP de GET a DELETE)
+ 
+  end
+
+  private
+ 
   def product_params
     params.require(:product).permit(:title, :description, :price) # Requirimos la existencia de un objeto "product" y permitimos solo la escritura de los campos permitidos
   end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -34,6 +34,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     } 
 
     assert_redirected_to products_path
+    assert_equal flash[:notice], 'Tu producto se ha creado correctamente.'
   end
 
   test 'does not allow to create a new product with empty fields' do
@@ -46,6 +47,45 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     } 
 
     assert_response :unprocessable_entity
+  end
+
+  test 'allow to edit a product' do
+    get edit_product_path(products(:ps4))
+
+    assert_response :success
+    assert_select 'form'
+  end
+
+  test 'allows to update a new product' do
+    patch product_path(products(:ps4)), params: {
+      product: {
+        price: 165
+        }
+    } 
+
+    assert_redirected_to product_path(products(:ps4))
+    assert_equal flash[:notice], 'Tu producto se ha actualizado correctamente.'
+  end
+
+  test 'does not allow to update a new product' do
+    patch product_path(products(:ps4)), params: {
+      product: {
+        price: nil
+        }
+    } 
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'can delete products' do 
+
+    assert_difference('Product.count', -1) do
+      delete product_path(products(:ps4))
+    end
+
+    assert_redirected_to products_path
+    assert_equal flash[:notice], 'Tu producto se ha eliminado correctamente.'
+
   end
 
 end
