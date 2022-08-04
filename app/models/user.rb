bec50671a65854178fb401c3bd3255dd315ccb7c
angelamadorfdez <angel@ingenio.xyz>
2022-08-04 15:59:17 +0200

    fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   
   has_secure_password
   
-  validates :email, presence: true; uniqueness: true
-  validates :username, presence: true; uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   validates :password_digest, length: { minimum: 6 }
 
 end
