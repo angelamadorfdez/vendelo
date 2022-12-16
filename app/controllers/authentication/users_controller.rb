7c35190b6043d5936150ff404f773d6afa6c8d29
angelamadorfdez <angel@ingenio.xyz>
2022-12-16 10:36:46 +0100

    Add FetchCountryJob

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -8,9 +8,10 @@ class Authentication::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.country = FetchCountryService.new(request.remote_ip).perform
-
+    # User background job instead: @user.country = FetchCountryService.new(request.remote_ip).perform
+    
     if @user.save
+      FetchCountryJob.perform_later(@user.id, request.remote_ip) # Background job call
       session[:user_id] = @user.id
       redirect_to products_path, notice: t('.created')
     else
